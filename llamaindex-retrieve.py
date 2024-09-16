42b850e9965673f4d33703aa82863401f4fff9d9
Alexander Wolff <wolffaxn@gmail.com>
2024-09-16 21:32:20 +0200

    feat(RAG): set temperature, top_k, top_p & similarity_top_k

--- llamaindex-retrieve.py
+++ llamaindex-retrieve.py
@@ -27,26 +27,36 @@ OLLAMA_BASE_URL = os.environ.get("OLLAMA_BASE_URL")
 OLLAMA_MODEL = os.environ.get("OLLAMA_MODEL")
 QDRANT_URL = os.environ.get("QDRANT_URL")
 QDRANT_COLLECTION_NAME = os.environ.get("QDRANT_COLLECTION_NAME")
+REQUEST_TIMEOUT = 120
 
 def query(query_text, collection_name):
 
     Settings.embed_model = OllamaEmbedding(
         model_name=EMBEDDING_MODEL,
         base_url=OLLAMA_BASE_URL,
-        request_timeout=600
+        request_timeout=REQUEST_TIMEOUT
     )
     Settings.llm = Ollama(
         model=OLLAMA_MODEL,
         base_url=OLLAMA_BASE_URL,
-        temperature=0,
-        top_k=1,
-        request_timeout=600
+        # The temperature of the model. Increasing the temperature will
+        # make the model answer more creatively.
+        temperature=0.0,
+        # Reduces the probability of generating nonsense. A higher value
+        # (e.g. 100) will give more diverse answers, while a lower value
+        # (e.g. 10) will be more conservative.
+        top_k=10,
+        # Works together with top-k. A higher value (e.g., 0.95) will lead
+        # to more diverse text, while a lower value (e.g., 0.5) will generate
+        # more focused and conservative text.
+        top_p=0.2,
+        request_timeout=REQUEST_TIMEOUT
     )
 
     # initialize Qdrant client
     client = QdrantClient(
         url=QDRANT_URL,
-        timeout=60
+        timeout=REQUEST_TIMEOUT
     )
     # initialize vector store
     vector_store = QdrantVectorStore(
@@ -56,7 +66,7 @@ def query(query_text, collection_name):
     index = VectorStoreIndex.from_vector_store(vector_store)
 
     query_engine = index.as_query_engine(
-        similarity_top_k=1,
+        similarity_top_k=5,
         vector_store_query_mode="default",
         streaming=True
     )
@@ -68,4 +78,5 @@ if __name__ == "__main__":
         query_text = sys.argv[1]
         collection_name = QDRANT_COLLECTION_NAME
 
-        print(query(query_text, collection_name))
+        result = query(query_text, collection_name)
+        print(result)
